72e9f70cf64baf3eaf7d04f434803e114e1df06a
jena6677 <jenaasutosh2000@gmail.com>
2022-01-19 23:42:34 +0530

    checking token

--- movie-info/src/main/java/com/movieinfo/movie/info/MovieInfoApplication.java
+++ movie-info/src/main/java/com/movieinfo/movie/info/MovieInfoApplication.java
@@ -2,7 +2,7 @@ package com.movieinfo.movie.info;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//hello
 @SpringBootApplication
 public class MovieInfoApplication {
 
